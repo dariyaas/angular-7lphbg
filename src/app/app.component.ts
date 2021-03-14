40c58c0eb66a35e369e37d51f0263406a2ceeb6a
dariyaas <dariyaseidaliyeva@gmail.com>
2021-03-14 13:05:08 +0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent { 
+  
+  github(){
+    window.open("https://github.com/dariyaas/Web-Development/blob/main/%D0%A7%D0%B5%D1%80%D0%BD%D0%BE%D0%B3%D0%BE%D1%80%D0%B8%D1%8F-%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B0-%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%BE%D0%B5-%D1%84%D0%BE%D1%82%D0%BE.jpg");
+  }
+
+  
+}
